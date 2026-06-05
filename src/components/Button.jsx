2b819ebf2c633bac70915e4b6946c29935c815bf
KARUNARATHNA W M D D <dilshankarunarathnastudy@gmail.com>
2026-06-05 23:41:31 +0530

    button component added

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({ children, textOnly, className }) {
+  let cssClasses = textOnly ? "text-button" : "button";
+
+  cssClasses += " " + className;
+
+  return <button className={cssClasses}>{children}</button>;
+}
